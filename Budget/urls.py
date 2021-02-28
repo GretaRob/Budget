4df1ef8501d80e66d466a862af371ffdaf7bc24a
GretaRob <greta.robichaud@gmail.com>
2021-02-28 22:35:02 +0700

    imported home function in url

--- Budget/urls.py
+++ Budget/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from BudgetApp.views import addPayments, addExpense
+from BudgetApp.views import home, addPayments, addExpense
 
 urlpatterns = [
     path('admin/', admin.site.urls),
