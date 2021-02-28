e6f20c01bad23e035763fe96320c409f30742a63
GretaRob <greta.robichaud@gmail.com>
2021-02-28 18:02:05 +0700

    added paths in urls

--- Budget/urls.py
+++ Budget/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from BudgetApp.views import addPayments, addExpense
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('addPayments/', addPayments, name='addPayments'),
+    path('addExpense/', addPayments, name='addPayments'),
 ]
